6f5041169cc286cbcc3f6e79740a06d8c37d6ad0
Yvonne Chen <yvonnechen1991@gmail.com>
2021-03-23 20:30:00 -0500

    commit tree 3010b6d09160ec6b1118cbefa4f039cbeec3272f

--- src/components/ToyCard.jsx
+++ src/components/ToyCard.jsx
@@ -3,11 +3,12 @@ import React, { Component } from 'react';
 class ToyCard extends Component {
 
   render() {
+    // debugger
     return (
       <div className="card">
-        <h2>{'' /* Toy's Name */}</h2>
-        <img src={'' /* Toy's Image */} alt={'' /* Toy's Name */} className="toy-avatar" />
-        <p>{'' /* Toy's Likes */} Likes </p>
+        <h2>{ this.props.toy.name/* Toy's Name */}</h2>
+        <img src={this.props.toy.image /* Toy's Image */} alt={'' /* Toy's Name */} className="toy-avatar" />
+        <p>{this.props.toy.likes /* Toy's Likes */} Likes </p>
         <button className="like-btn">Like {'<3'}</button>
         <button className="del-btn">Donate to GoodWill</button>
       </div>
--- src/components/ToyContainer.jsx
+++ src/components/ToyContainer.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ToyCard from './ToyCard'
 
-const ToyContainer = () => {
+const ToyContainer = ({ toys }) => {
   return(
     <div id="toy-collection">
-      {/* Render the collection of ToyCards */}
+      {/* Render the collection of ToyCards */
+        toys.map(toy => <ToyCard toy={ toy }/>)
+      })
     </div>
   );
-}
+} 
 
 export default ToyContainer;
